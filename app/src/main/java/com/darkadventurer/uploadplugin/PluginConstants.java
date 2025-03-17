13dcfdf65d242e6a9648db18aac283ae2c74044d
Jade <737338920@qq.com>
2025-03-17 15:35:33 +0800

    修复kotlin类问题

---
+++ app/src/main/java/com/darkadventurer/uploadplugin/PluginConstants.java
@@ -0,0 +1,19 @@
+package com.darkadventurer.uploadplugin;
+
+/**
+ * Description :
+ *
+ * @author Jade 2025/3/17
+ */
+public class PluginConstants {
+    public static final String  TASK_GROUP_NAME= "publishToZealot";
+    public static final String  TASK_DES= "tools of upload to Zealot";
+    public static final String  UPLOAD_PARAMS_NAME= "uploadZealotParams";
+    public static final String  DING_PARAMS_NAME= "buildDingParams";
+    public static final String  FEISHU_PARAMS_NAME= "buildFeiShuParams";
+    public static final String  WEIXIN_GROUP_PARAMS_NAME= "buildWeixinGroupParams";
+    public static final String  GIT_LOG_PARAMS_NAME= "buildGitLogParams";
+    public static final String  ANDROID_EXTENSION_NAME= "android";
+    public static final String  TASK_EXTENSION_NAME= "ApkBuildUploadZealot";
+    public static final String  TASK_EXTENSION_NAME_ONLY_UPLOAD= "OnlyUploadApkToZealot";
+}
