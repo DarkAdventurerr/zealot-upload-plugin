425b479299ff789e8b811e2bcbfb5211b531201c
Jade <737338920@qq.com>
2025-03-17 16:47:04 +0800

    更新库

--- app/src/main/java/com/dragonpass/intlapp/PluginConstants.java
+++ app/src/main/java/com/dragonpass/intlapp/PluginConstants.java
@@ -9,10 +9,10 @@ public class PluginConstants {
     public static final String  TASK_GROUP_NAME= "publishToZealot";
     public static final String  TASK_DES= "tools of upload to Zealot";
     public static final String  UPLOAD_PARAMS_NAME= "uploadZealotParams";
-    public static final String  DING_PARAMS_NAME= "buildDingParams";
-    public static final String  FEISHU_PARAMS_NAME= "buildFeiShuParams";
-    public static final String  WEIXIN_GROUP_PARAMS_NAME= "buildWeixinGroupParams";
-    public static final String  GIT_LOG_PARAMS_NAME= "buildGitLogParams";
+    public static final String  DING_PARAMS_NAME= "zealotBuildDingParams";
+    public static final String  FEISHU_PARAMS_NAME= "zealotBuildFeiShuParams";
+    public static final String  WEIXIN_GROUP_PARAMS_NAME= "zealotBuildWeixinGroupParams";
+    public static final String  GIT_LOG_PARAMS_NAME= "zealotBuildGitLogParams";
     public static final String  ANDROID_EXTENSION_NAME= "android";
     public static final String  TASK_EXTENSION_NAME= "ApkBuildUploadZealot";
     public static final String  TASK_EXTENSION_NAME_ONLY_UPLOAD= "OnlyUploadApkToZealot";
